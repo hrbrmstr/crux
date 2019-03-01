3c5cc718c44f51f83cb4a69c4ef035996c7e1aa2
boB Rudis <bob@rud.is>
2019-03-01 04:04:02 -0500

    R package repo initialization complete

---
+++ R/crux-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/crux>
+#' - BugReports: <https://gitlab.com/hrbrmstr/crux/issues>
+#' 
+#' @md
+#' @name crux
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
